60b5a204fa1063b788d1f5735ce510697b5cc0df
Arthur023 <48730631+Arthur023@users.noreply.github.com>
2024-04-15 13:16:31 +0200

    Update script.js
    Update token

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('https://delaware-12b-dev-ed.develop.my.salesforce.com/services/apexrest/SendJobApplication', {
         method: 'GET',
         headers: {
-            'Authorization': 'Bearer ' + '00DQy000006qNQz!AQEAQGTDnVvekME3AOxYN4qAseCHzEoC9Lbvti.c7EWA5Dl_JGYLEe7Jvct8TjGVNjXhZu0MLHU8A.1nltdYjo8xbMCo3G2B' 
+            'Authorization': 'Bearer ' + '00DQy000006qNQz!AQEAQKy3mFo5NMMruFik3s9EFRj.h0w6i2jCdbzPjUGecnK1iblZEeiz26zWT_8xbe636tlVQEOGCvQJ7VU2kK2bJV4Sfpot' 
         }
     })
     .then(response => {
@@ -93,7 +93,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + '00DQy000006qNQz!AQEAQGTDnVvekME3AOxYN4qAseCHzEoC9Lbvti.c7EWA5Dl_JGYLEe7Jvct8TjGVNjXhZu0MLHU8A.1nltdYjo8xbMCo3G2B' 
+                    'Authorization': 'Bearer ' + '00DQy000006qNQz!AQEAQKy3mFo5NMMruFik3s9EFRj.h0w6i2jCdbzPjUGecnK1iblZEeiz26zWT_8xbe636tlVQEOGCvQJ7VU2kK2bJV4Sfpot' 
                 },
                 body: JSON.stringify(requestData)
             })
